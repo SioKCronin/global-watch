9c401d339ca8962624721e889a0c7a40c9f41824
calvarado <calvarado@wesleyan.edu>
2017-02-05 00:04:29 -0800

    Made several changes to the file. I cleaned it up and created a main function.

--- spark/code/aggregator.py
+++ spark/code/aggregator.py
@@ -2,44 +2,54 @@ from operator import itemgetter
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 import pyspark_cassandra
-#from cassandra.cluster import Cluster
 
 SPARK_IP = "ip-172-31-2-11.us-west-2.compute.internal"
 SPARK_PORT = "7077"
 
 conf = SparkConf() \
        .setMaster("spark://%s:%s" % (SPARK_IP, SPARK_PORT))
-       #.set("spark.cassandra.connection.host", "cas-1")
 
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 path = "s3a://gdelt-open-data/events/20130530.export.csv"
 rdd = sc.textFile(path)
 
-date = 1
-country = 51
-e_type = 12
-num_mentions = 33
-SQLdate = 1
-arr_compressed = rdd.map(lambda x: x.split('\t')) \
-                    .map(lambda y: ((int(y[date]), y[country], y[e_type]), int(y[num_mentions]))) \
+def rddCleaning(rd):
+    date = 1
+    country = 51
+    event_c1 = 12
+    mentions = 33
+    event_c2 = 22
+
+    def fillin(tup):
+        if tup[2] == "" and tup[3] != "":
+            return ((tup[0],tup[1],tup[3],tup[4]))
+        else:
+            return ((tup[0],tup[1],tup[2],tup[4]))
+
+    rdd_reduce = rd.map(lambda x: x.split('\t')) \
+                    .map(lambda y: ((int(y[date]), y[country], y[event_c1], y[event_c2], int(y[mentions])))) \
+                    .map(fillin) \
+                    .map(lambda f: (((f[0],f[1],f[2]),f[3]))) \
                     .reduceByKey(lambda a,b: a+b)
 
-further_com = arr_compressed.map(lambda t:((t[0][1],t[0][0]),[(t[0][2],t[1])])) \
-                            .reduceByKey(lambda a, b: a+b) \
-                            .map(lambda v:(v[0],sorted(v[1],key=itemgetter(1), reverse=True))) \
-                            .map(lambda f:((f[0],f[1][:3]))) \
-                            .map(lambda g:((g[0],tuple(g[1])))) \
-                            .filter(lambda x: len(x[1]) == 3) \
-                            .map(lambda l: (l[0][0],l[0][1],l[1])) \
-                            .map(lambda k: (k[0],k[1],{k[2][0][0]:k[2][0][1],k[2][1][0]:k[2][1][1],k[2][2][0]:k[2][2][1]}))
+    rdd_format = rdd_reduce.map(lambda t:((t[0][1],t[0][0]),[(t[0][2],t[1])])) \
+                           .filter(lambda c: c[1][0][0] != "") \
+                           .reduceByKey(lambda a, b: a+b) \
+                           .map(lambda v:(v[0],sorted(v[1],key=itemgetter(1), reverse=True))) \
+                           .map(lambda f:((f[0],f[1][:5]))) \
+                           .map(lambda s: ((s[0],dict(s[1]))))
+
+    return rdd_format
+
+rdd_cleaned = rddCleaning(rdd)
 
 print("#########################################")
-print(further_com.take(6))
+print(rdd_cleaned.take(6))
 print("#########################################")
 
-#table_cass = sc.parallelize(further_com)
-further_com.saveToCassandra("gdelt","world_gather")
+#table_cass = sc.parallelize(rdd_format)
+rdd_cleaned.saveToCassandra("gdelt","world_gather")
 
 
 ##############
