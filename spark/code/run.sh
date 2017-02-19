248378646041c08652b423d6627c9ade4ab676c6
calvarado <calvarado@wesleyan.edu>
2017-02-18 16:11:21 -0800

    Added read.csv packege and modified ip addresses.

--- spark/code/run.sh
+++ spark/code/run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 spark-submit \
-    --master spark://ip-172-31-2-11:7077 \
+    --master spark://ip-***-**-*-**:7077 \
     --executor-memory 6G \
     --driver-memory 6G \
-    --packages TargetHolding/pyspark-cassandra:0.3.5 \
-    --conf spark.cassandra.connection.host=172.31.2.11,172.31.2.14,172.31.2.9,172.31.2.7 \
-    aggregator.py
-
-#make sure to do the command chmod +x run.sh
+    --packages TargetHolding/pyspark-cassandra:0.3.5,com.databricks:spark-csv_2.10:1.2.0 \
+    --conf spark.cassandra.connection.host=***.**.*.*,***.**.*.**,***.**.*.* \
+    second.py
